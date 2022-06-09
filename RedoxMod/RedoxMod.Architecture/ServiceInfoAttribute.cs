bb2942342b4c6ef68ae5e2916b320a51597fb5b4
MichielArkema <michiel.arkema@gmail.com>
2022-06-09 11:17:06 +0200

    Added the ServiceInfoAttribute.cs

---
+++ RedoxMod/RedoxMod.Architecture/ServiceInfoAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RedoxMod.Architecture
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ServiceInfoAttribute : Attribute
+    {
+        public string Alias { get;}
+        public ServiceLifetime ServiceLifetime { get; }
+        
+        public ServiceInfoAttribute(string alias, ServiceLifetime serviceLifetime)
+        {
+            Alias = alias;
+            ServiceLifetime = serviceLifetime;
+        }
+    }
+}
