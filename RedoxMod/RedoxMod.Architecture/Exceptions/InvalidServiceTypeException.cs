b3a76b5e43d96f0aa30c7d173f0d168f8da5bf81
MichielArkema <michiel.arkema@gmail.com>
2022-06-09 11:16:01 +0200

    Added exceptions for the container.

---
+++ RedoxMod/RedoxMod.Architecture/Exceptions/InvalidServiceTypeException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RedoxMod.Architecture.Exceptions
+{
+    public class InvalidServiceTypeException : Exception
+    {
+        public InvalidServiceTypeException() { }
+        public InvalidServiceTypeException(string message) : base(message) { }
+    }
+}
---
+++ RedoxMod/RedoxMod.Architecture/Exceptions/ServiceBindingException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RedoxMod.Architecture.Exceptions
+{
+    public class ServiceBindingException : Exception
+    {
+        public ServiceBindingException() {}
+        public ServiceBindingException(string message) : base(message) {}
+    }
+}
