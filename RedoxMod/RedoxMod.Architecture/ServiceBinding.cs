a652c232c5fa92bb88fed77399529b667827bf20
MichielArkema <michiel.arkema@gmail.com>
2022-06-09 11:17:20 +0200

    Added the ServiceBinding.cs

---
+++ RedoxMod/RedoxMod.Architecture/ServiceBinding.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace RedoxMod.Architecture
+{
+    /// <summary>
+    /// Represents a bound service.
+    /// </summary>
+    public sealed class ServiceBinding
+    {
+        /// <summary>
+        /// Gets the Service Type. (Interface or Abstract Class)
+        /// </summary>
+        public Type ServiceType { get; private set; }
+        
+        /// <summary>
+        /// Gets the Concrete (Implementation) type of the service.
+        /// </summary>
+        public Type ConcreteType { get; internal set; }
+        
+        /// <summary>
+        /// Gets the alias of this binding.
+        /// <para>A service alias can be used as an alternative to resolve services from the container.</para>
+        /// </summary>
+        public string Alias { get; }
+        
+        /// <summary>
+        /// Gets the life time of this service.
+        /// </summary>
+        public ServiceLifetime Lifetime { get; }
+        
+        /// <summary>
+        /// Gets the instance of the Service Implementation.
+        /// </summary>
+        public object? Instance { get; internal set; }
+        
+        /// <summary>
+        /// Gets if the service has been resolved or not.
+        /// </summary>
+        public bool Resolved { get; internal set; }
+        
+        public ServiceBinding(Type serviceType, Type concreteType, object? instance = null)
+        {
+            ServiceType = serviceType;
+            ConcreteType = concreteType;
+            Instance = instance;
+
+            ServiceInfoAttribute serviceInfo = concreteType.GetCustomAttribute<ServiceInfoAttribute>();
+            this.Alias = serviceInfo?.Alias ?? string.Empty;
+            this.Lifetime = serviceInfo?.ServiceLifetime ?? ServiceLifetime.Singleton;
+        }
+    }
+}
