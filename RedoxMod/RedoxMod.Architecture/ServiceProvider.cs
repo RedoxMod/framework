0ed1fb995df1a261c44c97031117b733c4c7d484
MichielArkema <michiel.arkema@gmail.com>
2022-06-12 16:49:04 +0200

    Added and created the ServiceProvider.cs

--- RedoxMod/RedoxMod.Architecture/ServiceProvider.cs
+++ RedoxMod/RedoxMod.Architecture/ServiceProvider.cs
@@ -1,7 +1,25 @@
-﻿namespace RedoxMod.Architecture
+﻿using System;
+using RedoxMod.API;
+
+namespace RedoxMod.Architecture
 {
     public abstract class ServiceProvider
     {
+        private readonly IApplication _app;
+
+        /// <summary>
+        /// Gets the service contracts of this provider.
+        /// </summary>
+        public virtual Type[] Contracts { get; }
         
+        protected ServiceProvider(IApplication app)
+        {
+            _app = app;
+        }
+        
+        /// <summary>
+        /// Register all the provider services.
+        /// </summary>
+        public virtual void Register() {}
     }
 }
