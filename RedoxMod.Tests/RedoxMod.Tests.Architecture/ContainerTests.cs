0687c2ee05e9d009829debbc19dd0f6cbe7ea10e
MichielArkema <michiel.arkema@gmail.com>
2022-06-12 13:16:31 +0200

    Added ContainerTests.cs to test the Dependency Container.

---
+++ RedoxMod.Tests/RedoxMod.Tests.Architecture/ContainerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using RedoxMod.Architecture;
+using RedoxMod.Tests.Architecture.Concretes;
+using RedoxMod.Tests.Architecture.Contracts;
+
+namespace RedoxMod.Tests.Architecture
+{
+    public class ContainerTests
+    {
+        private Container _container;
+        
+        [SetUp]
+        public void Setup()
+        {
+            this._container = new Container();
+            
+            this._container.Bind<ILogger, Logger>();
+        }
+        
+        [Test]
+        public void TestResolvingTheLoggerServiceFromTheContainer()
+        {
+            //Arrange
+            ILogger logger = this._container.Resolve<ILogger>();
+            
+            //Act
+            logger?.Log("Hello World!");
+            
+            //Assert
+            Assert.IsTrue(logger != null);
+        }
+    }
+}
