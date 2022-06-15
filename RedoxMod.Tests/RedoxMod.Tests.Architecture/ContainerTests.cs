8934e5440f04c3ed6b79c95e1d76d7943b5b5dad
MichielArkema <michiel.arkema@gmail.com>
2022-06-15 14:43:53 +0200

    Changes the location of where the bindings take place and Added another test to test the Container.Instance method.

--- RedoxMod.Tests/RedoxMod.Tests.Architecture/ContainerTests.cs
+++ RedoxMod.Tests/RedoxMod.Tests.Architecture/ContainerTests.cs
@@ -1,6 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using RedoxMod.API;
 using RedoxMod.Architecture;
+using RedoxMod.Core;
 using RedoxMod.Tests.Architecture.Concretes;
 using RedoxMod.Tests.Architecture.Contracts;
 
@@ -14,15 +17,14 @@ namespace RedoxMod.Tests.Architecture
         public void Setup()
         {
             this._container = new Container();
-            
-            this._container.Bind<ILogger, Logger>();
-            this._container.Bind<IMessageProvider, MessageProvider>();
         }
         
         [Test]
         public void TestResolvingTheLoggerServiceFromTheContainer()
         {
             //Arrange
+            this._container.Bind<ILogger, Logger>();
+            
             ILogger logger = this._container.Resolve<ILogger>();
             
             //Act
@@ -36,6 +38,7 @@ namespace RedoxMod.Tests.Architecture
         public void Resolve_And_Test_The_MessageProvider_To_See_If_Constructor_Injection_Works_Or_Not()
         {
             //Arrange
+            this._container.Bind<IMessageProvider, MessageProvider>();
             IMessageProvider messageProvider = this._container.Resolve<IMessageProvider>();
             
             //Act
@@ -44,6 +47,23 @@ namespace RedoxMod.Tests.Architecture
             
             //Assert
             Assert.NotNull(messageProvider);
+        }
+
+        [Test]
+        public void Bind_And_Resolve_An_Existing_Instance_Dependency()
+        {
+            //Arrange
+            this._container.Instance<IApplication>(new Application());
+            IApplication app = this._container.Resolve<IApplication>();
+            
+            //Act
+            
+            Console.WriteLine(app?.Version);
+
+            //Assert
+
+            Assert.NotNull(app);
+
         }
     }
 }
