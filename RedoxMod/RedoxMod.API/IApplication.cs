b5fe45965d56a47eeefa640c5524979680ffd781
MichielArkema <michiel.arkema@gmail.com>
2022-06-15 14:43:01 +0200

    Added some properties and the Initialize method.

--- RedoxMod/RedoxMod.API/IApplication.cs
+++ RedoxMod/RedoxMod.API/IApplication.cs
@@ -1,7 +1,43 @@
-﻿namespace RedoxMod.API
+﻿using RedoxMod.Architecture;
+using Semver;
+
+namespace RedoxMod.API
 {
-    public interface IApplication
+    public interface IApplication 
     {
+        /// <summary>
+        /// Gets the version of the application.
+        /// </summary>
+        SemVersion Version { get; }
         
+        /// <summary>
+        /// Gets the dependency container of this application.
+        /// </summary>
+        Container Container { get;}
+        
+        /// <summary>
+        /// Gets the custom path of the RedoxMod installation.
+        /// </summary>
+        string BasePath { get; }
+        
+        /// <summary>
+        /// Gets the path to the plugins directory.
+        /// </summary>
+        string PluginsPath { get; }
+        
+        /// <summary>
+        /// Gets the path to the language directory.
+        /// </summary>
+        string LangPath { get; }
+        
+        /// <summary>
+        /// Gets the path to the directory where the RedoxMod assemblies are located.
+        /// </summary>
+        string RootPath { get; }
+
+        /// <summary>
+        /// Initializes the application.
+        /// </summary>
+        void Initialize();
     }
 }
