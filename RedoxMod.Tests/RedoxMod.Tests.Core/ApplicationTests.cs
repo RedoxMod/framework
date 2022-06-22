a48087a29c8742f506ba239abb2d223e1731b9e3
MichielArkema <michiel.arkema@gmail.com>
2022-06-22 13:18:50 +0200

    Added the ApplicationTests file.

--- RedoxMod.Tests/RedoxMod.Tests.Core/ApplicationTests.cs
+++ RedoxMod.Tests/RedoxMod.Tests.Core/ApplicationTests.cs
@@ -1,10 +1,29 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using RedoxMod.API;
+using RedoxMod.Core;
 
 namespace RedoxMod.Tests.Core
 {
     [TestFixture]
     public class ApplicationTests
     {
+        private IApplication _app;
         
+        [SetUp]
+        public void Setup()
+        {
+            this._app = new Application();
+        }
+
+        [Test]
+        public void Test_The_Initialization_Of_The_Application()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+        }
     }
 }
